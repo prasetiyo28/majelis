8dda8423b8a2b40f9143a37fdc97ef52a426f248
Bayu Adi Prasetiyo <bayu28.bap@gmail.com>
2019-05-21 11:04:35 +0700

    fixing bug database

--- db/majelis.sql
+++ db/majelis.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 21, 2019 at 05:55 AM
+-- Generation Time: May 21, 2019 at 06:03 AM
 -- Server version: 10.1.29-MariaDB
 -- PHP Version: 7.2.0
 
@@ -21,6 +21,45 @@ SET time_zone = "+00:00";
 --
 -- Database: `majelis`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id_admin` int(11) NOT NULL,
+  `nama` varchar(120) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id_admin`, `nama`, `email`, `password`) VALUES
+(1, 'Fatkhul Falah', 'fatkhulfalah@gmail.com', '2b620bdf79fc91e5f062c2d6e897dd3c');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori` int(2) NOT NULL,
+  `kategori` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kategori`
+--
+
+INSERT INTO `kategori` (`id_kategori`, `kategori`) VALUES
+(1, 'Nadhatul Ulama'),
+(2, 'Muhammadiyah');
 
 -- --------------------------------------------------------
 
@@ -50,9 +89,48 @@ INSERT INTO `majelis` (`id_majelis`, `nama_majelis`, `ketua`, `alamat`, `kontak`
 (2, 'Arauddhotul Mahmoedah', 'Ahmad Syekh ', 'Jalan Teuku Umar No.30 Tegal', '0828382839128', 2, 'majelis_1558085386.png', 0, 0, 0),
 (3, 'Majelis Al Ikhlas', 'Falah', 'Jatibarang', '08543281777', 1, 'majelis_1558410730.png', 0, 0, 0);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id_user` int(11) NOT NULL,
+  `nama` varchar(150) NOT NULL,
+  `alamat` varchar(150) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `jenis_kelamin` varchar(9) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `id_majelis` int(11) NOT NULL DEFAULT '0',
+  `deleted` tinyint(1) NOT NULL DEFAULT '0',
+  `verif` tinyint(1) NOT NULL DEFAULT '0',
+  `blocked` tinyint(1) NOT NULL DEFAULT '0',
+  `level` tinyint(4) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id_user`, `nama`, `alamat`, `email`, `jenis_kelamin`, `password`, `id_majelis`, `deleted`, `verif`, `blocked`, `level`) VALUES
+(1, 'Aldin Tri Mulyo', 'Kalinyamat Kulon Gang 12 , Kota Tegal', 'aldin@gmail.com', 'Laki-Laki', '7a59124c390c24bd6f76035deea98c83', 1, 0, 1, 0, 0);
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`);
 
 --
 -- Indexes for table `majelis`
@@ -61,14 +139,38 @@ ALTER TABLE `majelis`
   ADD PRIMARY KEY (`id_majelis`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id_kategori` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `majelis`
 --
 ALTER TABLE `majelis`
   MODIFY `id_majelis` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
